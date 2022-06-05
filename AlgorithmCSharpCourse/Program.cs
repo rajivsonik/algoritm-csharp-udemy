e835e09c27910b9401e78b805ed379cbae4bebaa
rajivsonik <rajiv_sonik@yahoo.com>
2022-06-05 00:18:43 -0700

    4k records test added

--- AlgorithmCSharpCourse/Program.cs
+++ AlgorithmCSharpCourse/Program.cs
@@ -9,6 +9,7 @@ namespace AlgorithmCSharpCourse
     {
         static void Main(string[] args)
         {
+            // check 1k records
             var ints = ReadFile.ReadInts(@"data\1Kints.txt").ToArray();
             var watch = new Stopwatch();
             watch.Start();
@@ -16,8 +17,20 @@ namespace AlgorithmCSharpCourse
             var tripletsFound = ThreeSum.CountTriplet(ints);
             watch.Stop();
 
-            Console.WriteLine("Number of \"Zero Sum\" Triplets: " + tripletsFound);
-            Console.WriteLine($"Time Taken:{ watch.Elapsed:g}");
+            Console.WriteLine("Number of \"Zero Sum\" Triplets in 1k records: " + tripletsFound);
+            Console.WriteLine($"Time Taken in 1k records:{ watch.Elapsed:g}");
+
+
+            // check 4k records 
+            ints = ReadFile.ReadInts(@"data\4Kints.txt").ToArray();
+            watch = new Stopwatch();
+            watch.Start();
+
+            tripletsFound = ThreeSum.CountTriplet(ints);
+            watch.Stop();
+
+            Console.WriteLine("Number of \"Zero Sum\" Triplets in 4k records: " + tripletsFound);
+            Console.WriteLine($"Time Taken in 4k records:{ watch.Elapsed:g}");
         }
     }
 }
